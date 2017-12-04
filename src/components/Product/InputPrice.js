ae3219edfbed91502eb922734898be9fef33dc49
Bill Tuan <baotuan0.0@gmail.com>
2017-12-04 16:17:34 +0700

    Refactor product page

---
+++ src/components/Product/InputPrice.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import NumericInput from "react-numeric-input";
+
+const InputPrice = props => {
+  return (
+    <NumericInput
+      value={props.value}
+      precision={2}
+      size={6}
+      step={0.1}
+      mobile={false}
+      style={{
+        wrap: {
+          background: "#E2E2E2",
+          boxShadow: "0 0 1px 1px #fff inset, 1px 1px 5px -1px #000",
+          padding: "2px 2.26ex 2px 2px",
+          borderRadius: "6px 3px 3px 6px",
+          fontSize: 32
+        },
+        input: {
+          borderRadius: "4px 2px 2px 4px",
+          color: "#988869",
+          padding: "0.1ex 1ex",
+          border: "1px solid #ccc",
+          marginRight: 4,
+          display: "block",
+          fontWeight: 100
+        },
+        "input:focus": {
+          border: "1px inset #69C",
+          outline: "none"
+        },
+        arrowUp: {
+          borderBottomColor: "rgba(66, 54, 0, 0.63)"
+        },
+        arrowDown: {
+          borderTopColor: "rgba(66, 54, 0, 0.63)"
+        }
+      }}
+    />
+  );
+};
+
+export default InputPrice;
---
+++ src/components/Product/MenuInfo.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Tab, Menu, Label } from "semantic-ui-react";
+
+const panes = [
+  {
+    menuItem: { key: "details", icon: "info", content: "Description" },
+    render: () => <Tab.Pane>Tab 1 Content</Tab.Pane>
+  },
+  {
+    menuItem: (
+      <Menu.Item key="auction">
+        Auction History<Label>15</Label>
+      </Menu.Item>
+    ),
+    render: () => <Tab.Pane>Tab 2 Content</Tab.Pane>
+  },
+  {
+    menuItem: (
+      <Menu.Item key="review">
+        Reviews<Label>8</Label>
+      </Menu.Item>
+    ),
+    render: () => <Tab.Pane>Tab 1 Content</Tab.Pane>
+  }
+];
+
+export default () => <Tab panes={panes} />;
