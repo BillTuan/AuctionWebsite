c3a7df83f5e4d5c155a5e09ba49ac474c0584db1
Bill Tuan <baotuan0.0@gmail.com>
2017-12-06 19:55:27 +0700

    Fix table pending product seller id admin page

--- src/components/Admin/PendingPost.js
+++ src/components/Admin/PendingPost.js
@@ -55,79 +55,80 @@ class PendingPost extends Component {
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {this.props.products.map(({ id, name, description, img1 }) => {
-              return (
-                <Table.Row>
-                  <Table.Cell>{name}</Table.Cell>
-                  <Table.Cell>{description}</Table.Cell>
-                  {/* TODO: Fix "id" -> "Seller Id" */}
-                  <Table.Cell>{id}</Table.Cell>
-                  <Table.Cell>
-                    <Image src={img1} size="small" />
-                  </Table.Cell>
-                  <Table.Cell collapsing>
-                    {/* Approve confirm */}
-                    <Confirm
-                      open={this.state.openApproveConfirm}
-                      cancelButton="No"
-                      confirmButton="Yes"
-                      onCancel={this.approvehandleCancel}
-                      onConfirm={this.approvehandleConfirm}
-                    />
-                    <Button
-                      positive
-                      onClick={() => {
-                        this.showApproveConfirm();
-                      }}
-                    >
-                      Approve
-                    </Button>
-                    {/* This will show when Approve button clicked */}
-                    {/* Deny confirm */}
-                    <Modal
-                      trigger={
-                        <Button
-                          negative
-                          onClick={() => {
-                            this.setState({ openDenyConfirm: true });
-                          }}
-                        >
-                          Deny
-                        </Button>
-                      }
-                      open={this.state.openDenyConfirm}
-                    >
-                      <Modal.Header>Please type a reason below</Modal.Header>
-                      <Modal.Content>
-                        <div>
-                          <Form>
-                            <TextArea placeholder="Tell me why" />
-                          </Form>
-                        </div>
-                      </Modal.Content>
-                      <Modal.Actions>
-                        <Button
-                          onClick={() => {
-                            this.denyhandleCancel();
-                          }}
-                        >
-                          No
-                        </Button>
-                        <Button
-                          primary
-                          onClick={() => {
-                            this.denyhandleConfirm();
-                          }}
-                        >
-                          Yes
-                        </Button>
-                      </Modal.Actions>
-                    </Modal>
-                    {/* This will show when Deny button clicked */}
-                  </Table.Cell>
-                </Table.Row>
-              );
-            })}
+            {this.props.products.map(
+              ({ id, name, description, img1, seller }) => {
+                return (
+                  <Table.Row>
+                    <Table.Cell>{name}</Table.Cell>
+                    <Table.Cell>{description}</Table.Cell>
+                    <Table.Cell>{seller.id}</Table.Cell>
+                    <Table.Cell>
+                      <Image src={img1} size="small" />
+                    </Table.Cell>
+                    <Table.Cell collapsing>
+                      {/* Approve confirm */}
+                      <Confirm
+                        open={this.state.openApproveConfirm}
+                        cancelButton="No"
+                        confirmButton="Yes"
+                        onCancel={this.approvehandleCancel}
+                        onConfirm={this.approvehandleConfirm}
+                      />
+                      <Button
+                        positive
+                        onClick={() => {
+                          this.showApproveConfirm();
+                        }}
+                      >
+                        Approve
+                      </Button>
+                      {/* This will show when Approve button clicked */}
+                      {/* Deny confirm */}
+                      <Modal
+                        trigger={
+                          <Button
+                            negative
+                            onClick={() => {
+                              this.setState({ openDenyConfirm: true });
+                            }}
+                          >
+                            Deny
+                          </Button>
+                        }
+                        open={this.state.openDenyConfirm}
+                      >
+                        <Modal.Header>Please type a reason below</Modal.Header>
+                        <Modal.Content>
+                          <div>
+                            <Form>
+                              <TextArea placeholder="Tell me why" />
+                            </Form>
+                          </div>
+                        </Modal.Content>
+                        <Modal.Actions>
+                          <Button
+                            onClick={() => {
+                              this.denyhandleCancel();
+                            }}
+                          >
+                            No
+                          </Button>
+                          <Button
+                            primary
+                            onClick={() => {
+                              this.denyhandleConfirm();
+                            }}
+                          >
+                            Yes
+                          </Button>
+                        </Modal.Actions>
+                      </Modal>
+                      {/* This will show when Deny button clicked */}
+                    </Table.Cell>
+                  </Table.Row>
+                );
+              }
+            )}
           </Table.Body>
         </Table>
       </Grid.Column>
