4422ce50de19a381e2fd9a7594258b3281aa1f18
Bill <baotuan0.0@gmail.com>
2017-12-08 15:13:16 +0700

    Fix routes config server

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -9,6 +9,19 @@ Rails.application.routes.draw do
 
     # lấy dánh sách sản phẩm theo mã category
     get 'categories/:id/products', to: 'products#get_products_by_categoryid'
+
+    get 'admin/alluser', to: 'admin#all_user'
+
+    get 'users/:user_id/viewed-items', to: 'viewed_items#index'
+    post 'users/:user_id/viewed-items/:item_id', to: 'viewed_items#create'
+    delete 'users/:user_id/viewed-items/:item_id', to: 'viewed_items#destroy'
+
+    get 'users/:user_id/products', to: 'products#get_products_by_sellerid'
+    get 'users/:user_id/auctions', to: 'auction_detail#index'
+    get 'users/:user_id/bids', to: 'bid_success#index'
+    # get 'users/:id/profile', to: 'users#profile'
+
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
