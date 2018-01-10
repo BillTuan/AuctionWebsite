db5cc7d8ccabe05f5fbca4d7eb34fda5103b0e03
taikim8484 <primnumbe@gmail.com>
2018-01-10 16:32:57 +0700

    Added Login Admin Page

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
-import { Container, Grid, Menu } from "semantic-ui-react";
+import {
+  Container,
+  Grid,
+  Menu,
+  Button,
+  Form,
+  Header,
+  Image,
+  Message,
+  Segment
+} from "semantic-ui-react";
 import PendingPost from "./PendingPost";
 import Report from "./Report";
 import ListUser from "./ListUser";
-
 const contents = {
   PendingPost: <PendingPost />,
   Report: <Report />,
@@ -13,13 +22,57 @@ const contents = {
 class Admin extends Component {
   state = {
     activeItem: "PendingPost",
-    currentContent: contents.PendingPost
+    currentContent: contents.PendingPost,
+    isLogin: false
   };
   handleItemClick = (e, { name }) =>
     this.setState({ activeItem: name, currentContent: contents[name] });
   render() {
     const { activeItem, currentContent } = this.state;
-    return (
+    return !this.state.isLogin ? (
+      <Grid
+        textAlign="center"
+        style={{ height: "100%" }}
+        verticalAlign="middle"
+      >
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" color="teal" textAlign="center">
+            <Image src="https://react.semantic-ui.com/logo.png" /> Log-in to
+            your account
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                icon="user"
+                iconPosition="left"
+                placeholder="E-mail address"
+              />
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+              />
+              <Button
+                color="teal"
+                fluid
+                size="large"
+                onClick={() => {
+                  this.setState({ isLogin: true });
+                }}
+              >
+                Login
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            New to us? <a href="#">Sign Up</a>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    ) : (
       <Container>
         <Grid>
           <Grid.Column width={4}>
